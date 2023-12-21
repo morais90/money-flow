abcf10b26a630e9a5ed7a5a225b84abe90c558dc
Willian Morais <williandmorais@gmail.com>
2023-12-21 18:24:52 -0300

    chore: include the paument-rule router to composer-api

--- payment-composer-api/app/main.py
+++ payment-composer-api/app/main.py
@@ -1,8 +1,10 @@
+from app.payment_rule.api import router as payment_rule_router
 from fastapi import FastAPI
 
 app = FastAPI()
+app.include_router(payment_rule_router)
 
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+@app.get("/", status_code=200)
+async def health_check():
+    return {"status": "ok"}

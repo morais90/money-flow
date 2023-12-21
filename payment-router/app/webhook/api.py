9f34a57705e6ab85d6158ee0f3c307389b6d4875
Willian Morais <williandmorais@gmail.com>
2023-12-21 18:27:17 -0300

    chore: handle internal server errors on receive the payment event

--- payment-router/app/webhook/api.py
+++ payment-router/app/webhook/api.py
@@ -1,6 +1,7 @@
 from app.core.broker import push_to_queue
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
+from .exceptions import ServiceException
 from .models import PaymentEvent
 from .services import CompanyService, PaymentComposerService
 
@@ -9,8 +10,12 @@ router = APIRouter()
 
 @router.post("/payment", status_code=204, tags=["webhook"])
 async def payment_webhook(event: PaymentEvent):
-    company = await CompanyService().get_company(event.company_id)
-    composer_rules = await PaymentComposerService().get_rules(event.company_id)
+    try:
+        company = await CompanyService().get_company(event.company_id)
+        composer_rules = await PaymentComposerService().get_rules(event.company_id)
 
-    payload = {"company": company, "payment": event.dict(), "rules": composer_rules}
-    push_to_queue("payment", payload)
+        payload = {"company": company, "payment": event.dict(), "rules": composer_rules}
+        push_to_queue("payment", payload)
+
+    except ServiceException:
+        raise HTTPException(status_code=500, detail="Internal server error")

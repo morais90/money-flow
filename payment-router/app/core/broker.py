5872a5c8645d940ae58a9aff774f3221c76ebf0d
Willian Morais <williandmorais@gmail.com>
2023-12-24 07:50:27 -0300

    chore: set the push_to_queue to be durable messages

--- payment-router/app/core/broker.py
+++ payment-router/app/core/broker.py
@@ -2,12 +2,19 @@ import json
 
 import pika
 
+from app.core.settings import settings
+
 
 def push_to_queue(queue_name: str, payload: dict):
-    connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
+    connection = pika.BlockingConnection(pika.URLParameters(str(settings.RABBITMQ_DSN)))
     channel = connection.channel()
 
-    channel.queue_declare(queue=queue_name)
-    channel.basic_publish(exchange="", routing_key="hello", body=json.dumps(payload))
+    channel.queue_declare(queue=queue_name, durable=True)
+    channel.basic_publish(
+        exchange="",
+        routing_key="payment",
+        body=json.dumps(payload),
+        properties=pika.BasicProperties(delivery_mode=pika.DeliveryMode.Persistent),
+    )
 
     connection.close()

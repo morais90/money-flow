72daa9a5ca9009ec37803d82103aeab4e9d83d3a
Willian Morais <williandmorais@gmail.com>
2023-12-25 07:34:17 -0300

    chore: add health_check endpoint to payment-router

--- payment-router/app/main.py
+++ payment-router/app/main.py
@@ -1,5 +1,11 @@
-from app.webhook.api import router as webhook_router
 from fastapi import FastAPI
+
+from app.webhook.api import router as webhook_router
 
 app = FastAPI()
 app.include_router(webhook_router)
+
+
+@app.get("/", status_code=200)
+async def health_check():
+    return {"status": "ok"}

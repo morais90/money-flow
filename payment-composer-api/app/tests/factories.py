ad06324983322cb72d5f11aad8863088ff8f0891
Willian Morais <williandmorais@gmail.com>
2023-12-25 17:54:23 -0300

    test: remove the faker seed from Factories

--- payment-composer-api/app/tests/factories.py
+++ payment-composer-api/app/tests/factories.py
@@ -7,15 +7,14 @@ from app.core.models import PaymentRule
 
 from .database import ScopedSession
 
-Faker.seed(0)
 faker = Faker()
 
 
 class PaymentRuleFactory(SQLAlchemyModelFactory):
     id = factory.Sequence(lambda _: faker.uuid4())
     company_id = factory.Sequence(lambda _: faker.uuid4())
-    created_at = factory.Sequence(lambda i: pendulum.now("UTC").add(days=i))
-    updated_at = factory.Sequence(lambda i: pendulum.now("UTC").add(days=i))
+    created_at = factory.Sequence(lambda i: pendulum.now("UTC"))
+    updated_at = factory.Sequence(lambda i: pendulum.now("UTC"))
 
     class Meta:
         model = PaymentRule

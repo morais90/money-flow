676006f60802db0155b93f70180e00b390c6911c
Willian Morais <williandmorais@gmail.com>
2023-12-25 17:53:52 -0300

    test: add use of the POSTGRES_DSN env variable on setup the test database

--- payment-composer-api/app/tests/database.py
+++ payment-composer-api/app/tests/database.py
@@ -2,14 +2,18 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.pool import StaticPool
 
-engine = create_engine("postgresql://postgres:moneyflow@postgres:5432/test_db", poolclass=StaticPool)
+from app.core.settings import settings
+
+POSTGRES_DSN = str(settings.POSTGRES_DSN).replace("+asyncpg", "").rstrip("/moneyflow")
+
+engine = create_engine(POSTGRES_DSN, poolclass=StaticPool)
 
 Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 ScopedSession = scoped_session(Session)
 
 
 def create_database():
-    temporary_engine = create_engine("postgresql://postgres:moneyflow@postgres:5432", poolclass=StaticPool)
+    temporary_engine = create_engine(POSTGRES_DSN.rstrip("/test_db"), poolclass=StaticPool)
 
     with temporary_engine.connect().execution_options(isolation_level="AUTOCOMMIT") as connection:
         connection.execute(text("DROP DATABASE if exists test_db;"))

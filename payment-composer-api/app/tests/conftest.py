ae56590684ff6e98441cc35f04ef73a3335f4690
Willian Morais <williandmorais@gmail.com>
2023-12-25 16:26:24 -0300

    test: add freezegun fixture to tests

--- payment-composer-api/app/tests/conftest.py
+++ payment-composer-api/app/tests/conftest.py
@@ -1,6 +1,6 @@
-import pendulum
 import pytest
 from fastapi.testclient import TestClient
+from freezegun import freeze_time
 
 from app.core.models import Model
 from app.main import app
@@ -9,9 +9,13 @@ from .database import create_database, engine
 
 
 @pytest.fixture(scope="session", autouse=True)
-def freeze_time():
-    known = pendulum.datetime(2023, 12, 24, 12, tz="UTC")
-    pendulum.set_test_now(known)
+def freeze():
+    freezer = freeze_time("2023-12-24 12:00:00")
+    freezer.start()
+
+    yield
+
+    freezer.stop()
 
 
 @pytest.fixture(scope="session", autouse=True)

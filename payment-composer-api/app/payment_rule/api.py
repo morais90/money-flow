48f50da73c711c07c9d84456f59a0707d5f95f2b
Willian Morais <williandmorais@gmail.com>
2023-12-25 07:32:28 -0300

    chore: add return to create payment rule endpoint

--- payment-composer-api/app/payment_rule/api.py
+++ payment-composer-api/app/payment_rule/api.py
@@ -1,29 +1,35 @@
 from uuid import UUID
 
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy import select
+from sqlalchemy.orm.exc import NoResultFound
+
 from app.core.database import Session
 from app.core.dependencies import get_session
 from app.core.models import PaymentRule
-from fastapi import APIRouter, Depends
-from sqlalchemy import select
 
-from .schemas import PaymentRuleCreate
+from .schemas import PaymentRuleCreate, PaymentRuleSchema
 
 router = APIRouter()
 
 
 @router.get("/payment-rules/{company_id}")
-async def get_payment_rules(company_id: UUID, db: Session = Depends(get_session)):
+async def get_payment_rules(company_id: UUID, db: Session = Depends(get_session)) -> PaymentRuleSchema:
     statement = select(PaymentRule).where(PaymentRule.company_id == str(company_id))
-    payment_rule = (await db.scalars(statement)).one()
+
+    try:
+        payment_rule = (await db.scalars(statement)).one()
+    except NoResultFound:
+        raise HTTPException(status_code=404, detail="Not Found")
 
     return payment_rule
 
 
-@router.post("/payment-rules/")
-async def create_payment_rules(
-    payload: PaymentRuleCreate, db: Session = Depends(get_session)
-):
-    payment_rule = PaymentRule(**payload.dict())
+@router.post("/payment-rules/", status_code=201)
+async def create_payment_rules(payload: PaymentRuleCreate, db: Session = Depends(get_session)) -> PaymentRuleSchema:
+    payment_rule = PaymentRule(**payload.model_dump())
 
     async with db.begin():
         db.add(payment_rule)
+
+    return payment_rule

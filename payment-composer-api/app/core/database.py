3ad94314e276561043817b3c83a79e1b1084104e
Willian Morais <williandmorais@gmail.com>
2023-12-25 07:29:59 -0300

    style: apply formatter to database module

--- payment-composer-api/app/core/database.py
+++ payment-composer-api/app/core/database.py
@@ -1,26 +1,15 @@
 from asyncio import current_task
 
-from app.core.settings import settings
-from sqlalchemy.ext.asyncio import (
-    AsyncSession,
-    async_scoped_session,
-    create_async_engine,
-)
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, async_scoped_session, create_async_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
 
-engine = create_async_engine(
-    str(settings.POSTGRES_DSN),
-    echo=True,
-    connect_args={"server_settings": {"jit": "off"}},
-)
+from app.core.settings import settings
+
+engine = create_async_engine(str(settings.POSTGRES_DSN), echo=False, connect_args={"server_settings": {"jit": "off"}})
 async_session_factory = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    expire_on_commit=False,
-    bind=engine,
-    class_=AsyncSession,
+    autocommit=False, autoflush=False, expire_on_commit=False, bind=engine, class_=AsyncSession
 )
+
 Session = async_scoped_session(async_session_factory, scopefunc=current_task)
 
 Base = declarative_base()
